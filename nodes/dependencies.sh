93d9e7346db92a41fe5c92477c670d9c298aa933
yar0slavvv <128404628+yar0slavvv@users.noreply.github.com>
2023-05-05 17:59:26 -0100

    Update dependencies.sh

--- nodes/dependencies.sh
+++ nodes/dependencies.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-printGreen "Installing dependencies..." && sleep 1
+echo -e "\033[1;97;42mОновлення пакетів...\033[0m"
 sudo apt-get update &&
 sudo apt-get install -y curl iptables build-essential git lz4 wget jq make gcc nano chrony \
 tmux htop nvme-cli pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip libleveldb-dev &&
 
-printGreen "Installing go..." && sleep 1
+echo -e "\033[1;97;42mВстановлення GO...\033[0m"
 if ! [ -x "$(command -v go)" ]; then
   source <(curl -s "https://raw.githubusercontent.com/R1M-NODES/cosmos/master/utils/go_install.sh")
   source $HOME/.bash_profile
 fi
 
-printGreen "$(go version)"
